4b954a6ea6cab4b03f05ba0d3fa7d7057b1a7e2b
Jonathan Astwood <jastwood21@gmail.com>
2023-02-12 23:13:21 -0500

    Completed assignment

--- app/views.py
+++ app/views.py
@@ -1,5 +1,7 @@
 from app import app
 from flask import render_template, request, redirect, url_for, flash
+from datetime import datetime
+#Author: Jonathan Astwood
 
 
 ###
@@ -16,6 +18,10 @@ def home():
 def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
+
+@app.route('/profile/')
+def profile():
+    return render_template('profile.html', date = format_date_joined("2023-02-10"))
 
 
 ###
@@ -45,3 +51,10 @@ def add_header(response):
 def page_not_found(error):
     """Custom 404 page."""
     return render_template('404.html'), 404
+
+###
+# Regular functions
+###
+def format_date_joined(date):
+    Date = datetime.strptime(date, '%Y-%m-%d')
+    return Date.strftime("%b, %Y")
